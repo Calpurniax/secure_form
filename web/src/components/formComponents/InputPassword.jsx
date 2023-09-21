965de6b7694aae6ae1951d2ffed98b4339da1ce3
calpurniax <sofia.gracia@gmail.com>
2023-09-21 14:33:49 +0200

    git test

--- web/src/components/formComponents/InputPassword.jsx
+++ web/src/components/formComponents/InputPassword.jsx
@@ -7,10 +7,10 @@ const InputPassword = ({ cssStyle, labelText, id, register, errors }) => {
                     value:true,
                     message: "Password is required"
                 },
-                // pattern:{
-                //     value:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                //     message: "Password needs to have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"
-                // }                
+                pattern:{
+                    value:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                    message: "Password needs to have minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"
+                }                
             })} />
             {errors[id] && (<p>{errors[id]?.message}</p>)}
         </div>
