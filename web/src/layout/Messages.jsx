3224c7c16759f1f8e3201a167ac82fccbcf24720
Calpurniax <sofia.gracia@gmail.com>
2023-09-24 11:55:42 +0200

    add more delete message logic

--- web/src/layout/Messages.jsx
+++ web/src/layout/Messages.jsx
@@ -1,36 +1,44 @@
-import { getMessages, deleteMessage} from '../services/msgAndUsers';
+import { getMessages, deleteMessage } from '../services/msgAndUsers';
 import { useEffect, useState } from 'react';
 import MessageArticle from '../components/MessageArticle';
 
 const Messages = () => {
+  const [messages, setMessages] = useState([]);
 
-    const [messages, setMessages] = useState([])
+  useEffect(() => {
+    getMessages().then((response) => {
+      setMessages(response);
+    });
+  }, []);
 
-    useEffect(() => {
-        getMessages().then((response) => {
-            setMessages(response)
-        })
-    }, [])
+  const handleDelete = (id) => {
+    const messagePosition = messages.findIndex((each) => each._id === id);
+    if (messagePosition === -1) return console.log('no messages');
+    deleteMessage(id);
+    return setMessages(messages.splice(messagePosition, 1));
+  };
 
-    const handleDelete=(id)=>{        
-        deleteMessage(id)
-    }
+  const renderMsg = () => {
+    if (messages.length > 0) {
+      return messages.map((eachMessage) => {
+        return (
+          <MessageArticle
+            eachMessage={eachMessage}
+            key={eachMessage._id}
+            handleDelete={handleDelete}
+          />
+        );
+      });
+    } else return <p>No hay mensajes</p>;
+  };
 
-    const renderMsg = () => {
-        if (messages.length > 0) {
-            return messages.map(eachMessage => {
-                return <MessageArticle eachMessage={eachMessage} key={eachMessage._id} handleDelete={handleDelete}/>
-            })
-        } else return <p>No hay mensajes</p>
-    }
-    
-    return (
-        <main>
-            <section>
-                <h1>Messages</h1>
-                <ul>{renderMsg()}</ul>
-            </section>
-        </main>
-    )
-}
-export default Messages
+  return (
+    <main>
+      <section>
+        <h2>Messages</h2>
+        <ul>{renderMsg()}</ul>
+      </section>
+    </main>
+  );
+};
+export default Messages;
